875ee9bd3c9b9c54d8c78c1782d72560fe45888a
Richard Weston <Richard.Weston@rackspace.co.uk>
2017-10-25 22:14:51 +0100

    Made comments section a bit clearer

--- Snippets/Get-ADAudit.ps1
+++ Snippets/Get-ADAudit.ps1
@@ -2,27 +2,47 @@ function Get-ADAudit () {
     Param(
         ## Parameter Child Domain
         [Parameter(Mandatory = $false)]
-        [ValidateSet("DFW", "HKG", "IAD", "LON", "ORD", "SYD", "RSGlobal")]
+        [ValidateSet("DFW", "HKG", "IAD", "LON", "ORD", "SYD", "GlobalRS")]
         [string]
-        $Domain,
+        $ChildDomain,
         ## Parameter Account Number
         [Parameter(Mandatory = $false)]
         [int32]
         $Account
     )
+    ## Module Import
+    Import-Module ActiveDirectory
     ## Parameter Domain Input/Validation
-    if (!($Domain -eq $null)) {
+    if (!($ChildDomain -eq $null)) {
         do {
             try {
-                [ValidateSet("DFW", "HKG", "IAD", "LON", "ORD", "SYD","RSGlobal")]$Domain = Read-Host -Prompt "Enter Domain (e.g. DFW/HKG/IAD/LON/ORD/SYD/RSGlobal)"
+                [ValidateSet("DFW", "HKG", "IAD", "LON", "ORD", "SYD","GlobalRS")]$ChildDomain = Read-Host -Prompt "Enter Domain (e.g. DFW/HKG/IAD/LON/ORD/SYD/GlobalRS)"
             } catch {}
         } until ($?)
     }
-    ## Check for Intensive/RSGlobal
-    if ($Domain -eq "RSGlobal") {
-        $DistinguishedName = $Domain + "RS.Global"
+    ## Check for Intensive/GlobalRS
+    if ($ChildDomain -eq "GlobalRS") {
+        $Server = $ChildDomain + ".Rack.Space"
     } else {
-        $DistinguishedName = $Domain + "intensive.int"
+        $Server = $ChildDomain + ".intensive.int"
+    }    
+    ## DNS Array
+    $FullDomain = $Server.Split('.')
+    foreach ($v in $FullDomain) {
+        $FullDomain += "DC=" + $v
+        $FullDomain = $FullDomain -ne $v
+    }
+    ## DNS/DN String
+    $FullDomain = $FullDomain -join ","
+    ## Corrects OU Structure based on City
+    switch ($ChildDomain) {
+        "DFW"       { $City = "OU=Rax,OU=Dallas"    }
+        "HKG"       { $City = "OU=Rax,OU=HongKong"  }
+        "IAD"       { $City = "OU=Rax,OU=Dulles"    }
+        "LON"       { $City = "OU=Rax,OU=London"    }
+        "ORD"       { $City = "OU=Rax,OU=Chicago"   }
+        "SYD"       { $City = "OU=Rax,OU=Sydney"    }
+        "GlobalRS"  { $City = "OU=Rax"              }
     }
     ## Parameter Account Input/Validation
     if (!($Account -eq $null)) {
@@ -31,16 +51,11 @@ function Get-ADAudit () {
                 [ValidatePattern('[0-9]')]$Account = Read-Host -Prompt "Enter Account Number"
             }
             catch {}
-        } until (($?) -and (Get-ADOrganizationalUnit -Identity ""))
-        
-    }    
-    ## Distinguished Name Array
-    $DistinguishedName = $DistinguishedName.Split('.')
-    foreach ($v in $DistinguishedName) {
-        $DistinguishedName += "DC=" + $v
-        $DistinguishedName = $DistinguishedName -ne $v
+        } until (($?) -and (Get-ADOrganizationalUnit -Server $Server -Identity "OU=$Account,$City,$FullDomain"))
     }
-    
-    $Output = Get-ADUser -Server "$Domain.intensive.int" -SearchBase "OU=$Account,OU=Rax,OU=Dallas,DC=$DistinguishedName[0],DC=,DC=" -Filter * -Properties *
+    ## Distinguished Name
+    $DistinguishedName = "OU=" + $Account + "," + $City + "," + $FullDomain
+    ## Output
+    $Output = Get-ADUser -Server $Server -SearchBase $DistinguishedName -Filter * -Properties *
     $Output | Format-Table -AutoSize Name, Enabled
 }
